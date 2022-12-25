7880c8723bc79cc869ca925c0fce5eec26b32fc9
cistT <cist.b220.t.chitose@gmail.com>
2022-12-25 11:14:26 +0900

    アプリのモジュールを設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './favorite/prisma.module';
+
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
