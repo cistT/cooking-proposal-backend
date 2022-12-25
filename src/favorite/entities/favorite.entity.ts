249506416c10fb2455363175f69a0656cc99010d
cistT <cist.b220.t.chitose@gmail.com>
2022-12-25 11:08:28 +0900

    レスポンスの型を定義

---
+++ src/favorite/entities/favorite.entity.ts
@@ -0,0 +1,48 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class FavoriteEntity {
+  @ApiProperty()
+  foodImageUrl: string;
+
+  @ApiProperty()
+  mediumImageUrl: string;
+
+  @ApiProperty()
+  nickname: string;
+
+  @ApiProperty()
+  pickup: number;
+
+  @ApiProperty()
+  rank: string;
+
+  @ApiProperty()
+  recipeCost: string;
+
+  @ApiProperty()
+  recipeDescription: string;
+
+  @ApiProperty()
+  recipeId: number;
+
+  @ApiProperty()
+  recipeIndication: string;
+
+  @ApiProperty()
+  recipeMaterial: string[];
+
+  @ApiProperty()
+  recipePublishday: Date;
+
+  @ApiProperty()
+  recipeTitle: string;
+
+  @ApiProperty()
+  recipeUrl: string;
+
+  @ApiProperty()
+  shop: number;
+
+  @ApiProperty()
+  smallImageUrl: string;
+}
